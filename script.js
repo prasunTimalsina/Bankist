839cac48cd769e61e551a5a0d9d9721d859e7e99
prasunTimalsina <timalsinaprasun61@gmail.com>
2024-05-05 14:04:04 +0545

    Add username to account objects

--- script.js
+++ script.js
@@ -81,97 +81,14 @@ const displayMovements = function (movements) {
 
 displayMovements(account1.movements);
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
 
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
-
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-/////////////////////////////////////////////////
-
-/*
-let arr = ['a', 'b', 'c', 'd', 'e'];
-
-//SLICE - create a copy doesn't change on new array.
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4)); //all operations same as strings
-
-//SPLICE - same as slice but change the original array i.e. extracted elements are gone from original array.
-console.log(arr.splice(1, 2)); //here the second parameter is not the range but the no. of element from index 1 to be  extracted.
-console.log(arr);
-
-//REVERSE - it mutate the orignal array , simply reverse the original array
-arr = ['a', 'b', 'c', 'd', 'e'];
-const arr2 = ['j', 'i', 'h', 'g', 'f'];
-console.log(arr2.reverse());
-console.log(arr2);
-
-//CONCAT
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-//JOIN
-console.log(letters.join(' '));
-
-*/
-/*
-///AT Method
-
-const arr = [12, 42, 34];
-
-//getting last element from array
-console.log(arr.at(-1)); // it is particulary use for this case cause it accept the negative index value
-//also used for the method chaining
-
-console.log('Prasun'.at(-1));
-*/
-
-/////LOOPING ARRAY forEach
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
-//for of
-
-for (const [i, movement] of movements.entries()) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
-  }
-}
-
-//It simply call the passed function for each iteration on the array.
-console.log('----FOREACH----');
-movements.forEach(function (movement, index, array) {
-  //Here parameter order matters and we cant use break and continue.
-  if (movement > 0) {
-    console.log(`Movement ${index + 1}: You deposited ${movement}`);
-  } else {
-    console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
-  }
-});
-
-//MAP
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
-
-currencies.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
-});
-
-//SET
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
-console.log(currenciesUnique);
-
-currenciesUnique.forEach(function (value, _, map) {
-  console.log(`${value}: ${value}`);
-});
+createUsernames(accounts);
